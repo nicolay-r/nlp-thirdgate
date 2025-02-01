eb3704f57b28504a45099ce5378a5aa6296fe1cd
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-02-01 13:28:34 +0000

    Updated

--- llm/transformers_llama.py
+++ llm/transformers_llama.py
@@ -7,20 +7,18 @@ from bulk_chain.core.llm_base import BaseLM
 
 class Llama32(BaseLM):
 
-    def __init__(self, model_name="meta-llama/Llama-3.2-3B-Instruct", api_token=None,
-                 temp=0.1, device='auto', max_length=None, use_bf16=False, **kwargs):
+    def __init__(self, model_name, api_token=None, temp=0.1, device='auto',
+                 max_length=32768, use_bf16=False, **kwargs):
         super(Llama32, self).__init__(name=model_name, **kwargs)
 
         self.__max_length = max_length
-        self.__pipe = pipeline(
-            "text-generation",
-            model=model_name,
-            torch_dtype=torch.bfloat16 if use_bf16 else "auto",
-            device_map=device,
-            temperature=temp,
-            pad_token_id=128001,
-            token=api_token
-        )
+        self.__pipe = pipeline("text-generation",
+                               model=model_name,
+                               torch_dtype=torch.bfloat16 if use_bf16 else "auto",
+                               device_map=device,
+                               temperature=temp,
+                               pad_token_id=128001,
+                               token=api_token)
 
     def ask(self, prompt):
         outputs = self.__pipe(
