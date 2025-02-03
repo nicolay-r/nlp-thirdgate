c94033508d9cf37a43d590ad6182b40ecebfa744
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-02-03 10:40:48 +0000

    Update llm_qwen_25_max_chat.sh

--- tutorials/llm_qwen_25_max_chat.sh
+++ tutorials/llm_qwen_25_max_chat.sh
@@ -3,7 +3,7 @@
 # bulk_chain version 0.25.0
 python3 -m bulk_chain.infer \
     --schema "default.json" \ 
-    --adapter "openai_156.py:OpenAIGPT" \
+    --adapter "dynamic:openai_156.py:OpenAIGPT" \
     %%m \
     --model_name "qwen-max-2025-01-25" \
     --assistant_prompt "You are a helpful assistant." \
