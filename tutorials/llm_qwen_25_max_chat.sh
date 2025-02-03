5de3767e2b5cc9e5d2b01e77646a928d0fc3dd21
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-02-03 10:37:23 +0000

    Create llm_qwen_25_max_chat.sh

---
+++ tutorials/llm_qwen_25_max_chat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This is an example of using OpenAI API wrapper for launching using 
+# bulk_chain version 0.25.0
+python3 -m bulk_chain.infer \
+    --schema "default.json" \ 
+    --adapter "openai_156.py:OpenAIGPT" \
+    %%m \
+    --model_name "qwen-max-2025-01-25" \
+    --assistant_prompt "You are a helpful assistant." \
+    --max_tokens 1024 \
+    --base_url "https://dashscope-intl.aliyuncs.com/compatible-mode/v1" \
+    --api_token "<PLATFORM-API-TOKEN>"
