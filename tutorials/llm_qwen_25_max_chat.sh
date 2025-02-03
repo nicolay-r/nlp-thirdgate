749365d6418232cd50b601baf282061c7120bc3b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-02-03 10:42:02 +0000

    Update llm_qwen_25_max_chat.sh

--- tutorials/llm_qwen_25_max_chat.sh
+++ tutorials/llm_qwen_25_max_chat.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # This is an example of using OpenAI API wrapper for launching using 
 # bulk_chain version 0.25.0
+# Max output tokens: https://www.reddit.com/r/LocalLLaMA/comments/1grgb3y/max_new_token_max_value_for_qwen25coder32binstruct/?rdt=41483
 python3 -m bulk_chain.infer \
     --schema "default.json" \ 
     --adapter "dynamic:openai_156.py:OpenAIGPT" \
     %%m \
     --model_name "qwen-max-2025-01-25" \
     --assistant_prompt "You are a helpful assistant." \
-    --max_tokens 1024 \
+    --max_tokens 8192 \
     --base_url "https://dashscope-intl.aliyuncs.com/compatible-mode/v1" \
     --api_token "<PLATFORM-API-TOKEN>"
