b715584b928fbe63dd3737612c4084fb828f655a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-04-07 10:34:44 +0100

    Added assistant customization feature for `replicate.io` models

--- llm/replicate_104.py
+++ llm/replicate_104.py
@@ -6,10 +6,12 @@ from bulk_chain.core.utils import auto_import
 
 class Replicate(BaseLM):
 
-    LLaMA3_instruct_prompt_template = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+    LLaMA3_instruct_prompt_template = (f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+                                       "{template}<|eot_id|><|start_header_id|>user<|end_header_id|>"
+                                       "\n\n{{prompt}}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n")
 
     @staticmethod
-    def get_template(max_tokens, temp, top_k=50):
+    def get_template(max_tokens, temp, top_k=50, template=""):
         return {
             "deepseek-ai/deepseek-r1": {
                 "top_p": 1.0,                                        # Default parameter at replicate.com demo
@@ -18,7 +20,7 @@ class Replicate(BaseLM):
                 "temperature": 0.6 if temp is None else temp,        # According to the DeepSeek documentation.
                 "max_tokens": min(max_tokens, 20480),
                 "prompt_template": "",                               # According to the DeepSeek documentation.
-            },           
+            },
             "meta/meta-llama-3-70b-instruct": {
                 "top_k": top_k,
                 "min_tokens": 0,
@@ -26,25 +28,25 @@ class Replicate(BaseLM):
                 "frequency_penalty": 0.2,
                 "temperature": 0.1 if temp is None else temp,
                 "max_tokens": max_tokens,
-                "prompt_template": Replicate.LLaMA3_instruct_prompt_template
+                "prompt_template": Replicate.LLaMA3_instruct_prompt_template.format(template=template)
             },
-             "meta/meta-llama-3-8b-instruct": {
+            "meta/meta-llama-3-8b-instruct": {
                 "top_k": top_k,
                 "top_p": 0.9,
                 "length_penalty": 1,
                 "presence_penalty": 1.15,
                 "temperature": 0.1 if temp is None else temp,
                 "max_tokens": max_tokens,
-                "prompt_template": Replicate.LLaMA3_instruct_prompt_template,
+                "prompt_template": Replicate.LLaMA3_instruct_prompt_template.format(template=template),
             },
         }
 
     def __init__(self, model_name, temp=None, max_tokens=512, api_token=None, stream=False,
-                 suppress_httpx_log=True, **kwargs):
+                 suppress_httpx_log=True, assistant="You are a helpful assistant", **kwargs):
         super(Replicate, self).__init__(model_name)
         self.r_model_name = model_name
 
-        all_settings = self.get_template(max_tokens=max_tokens, temp=temp)
+        all_settings = self.get_template(max_tokens=max_tokens, temp=temp, template=assistant)
 
         if model_name not in all_settings:
             raise Exception(f"There is no predefined settings for `{model_name}`. Please, Tweak the model first!")
@@ -67,4 +69,3 @@ class Replicate(BaseLM):
 
         chunks_it = self.client.stream(self.r_model_name, input=input_dict)
         return chunks_it if self.__stream else "".join([str(chunk) for chunk in chunks_it])
-        
